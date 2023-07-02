baefb60a42849d1837a8dcb3312eecf404e04d06
Viktoria Gornostaeva <vv.gornostaeva@mail.ru>
2023-07-02 11:24:33 +0200

    Удалены лишние пустые строки в импортах

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,7 +5,6 @@ from rest_framework import mixins, permissions, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
 from api.filters import IngredientFilter, RecipeFilter
 from api.permissions import IsSuperUserAdminAuthorOrReadOnly
 from api.serializers import (FavoriteSerializer, IngredientSerializer,
