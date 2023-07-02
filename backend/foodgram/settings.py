8d34c0caa58b28666fbb856c059044e97f74d6d8
Viktoria Gornostaeva <vv.gornostaeva@mail.ru>
2023-07-02 09:55:16 +0200

    Добавлены файлы для работы сайта на сервере

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -136,3 +136,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
