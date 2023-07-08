58fcc5486154a72f99634afa0be9f82b9a9edb7e
Viktoria Gornostaeva <vv.gornostaeva@mail.ru>
2023-07-08 20:40:18 +0200

    Добавлен валидатор для названия рецептов

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -66,14 +66,10 @@ class Recipe(models.Model):
     )
     name = models.CharField(
         'Название рецепта',
-        max_length=200
-        # validators=[
-        #     validators.RegexValidator(
-        #         regex='^.*[а-яА-Я]+.*$',
-        #         message='Название рецепта не должно состоять только '
-        #                 'из символов и цифр!'
-        #     )
-        # ],
+        max_length=200,
+        validators=[
+            validators.RegexValidator(r'^[А-Яа-яЁё]+$',)
+        ]
     )
     text = models.TextField(
         'Описание рецепта'
@@ -148,7 +144,7 @@ class IngredientInRecipe(models.Model):
             validators.MaxValueValidator(
                 10000,
                 message='Количество должно быть не больше 10000'
-            ),
+            )
         ]
     )
 
